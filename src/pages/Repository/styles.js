d2cadc95ba4141db90e38baa0fe195dec5a2d267
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2019-09-19 10:03:27 -0400

    Desafio 06 bootcamp

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Header = styled.View``;
+
+export const Name = styled.Text``;
